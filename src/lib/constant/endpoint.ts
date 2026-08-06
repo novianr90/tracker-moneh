0863994e17b02f1aee5dee5e021781d169bfae29
Novian <novian19@upi.edu>
2026-08-06 20:57:18 +0700

    delete: unnecessary file

--- src/lib/constant/endpoint.ts
+++
@@ -1,4 +0,0 @@
-import * as env from '$env/static/public';
-
-const SUPABASE_URL = (env as any).PUBLIC_SUPABASE_URL || 'http://127.0.0.1:54321';
-export const REST_SUPABASE_API = SUPABASE_URL + "/rest/v1/"
