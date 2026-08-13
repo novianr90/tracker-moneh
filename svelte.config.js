60e6b2ef9a337105cb0df45c59872000fe4e066c
Novian <novian19@upi.edu>
2026-08-13 10:14:40 +0700

    fix: use specific adapter for vercel or own infra

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterAuto from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isVercel = !!process.env.VERCEL;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: isVercel ? adapterAuto() : adapterNode(),
 		alias: {
 			$lib: './src/lib'
 		}
